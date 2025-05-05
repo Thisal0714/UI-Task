74fdc1293d7ae9b9e85527aefb3f57e188d2d27e
Thisal Chamodya <liyanaa-ps20022@stu.kln.ac.lk>
2025-05-05 17:13:17 +0530

    Login Page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,103 @@
 import Image from "next/image";
+import logo from "./logo.png";
+import image from "./Image.png";
 
 export default function Home() {
   return (
-    <h1 className="text-xl font-extrabold underline">
-      Hello world!
-    </h1>
+    <div className="w-full min-h-screen rounded-xl bg-black p-4 m-1">
+      <div className="flex flex-col md:flex-row w-full max-w-4xl mx-auto">
+
+        {/* Left Section */}
+        <div className="w-full md:w-1/2 p-4 flex flex-col items-start mt-5">
+          <Image src={logo} alt="logo" className="w-32 h-32 object-contain" />
+          <h1 className="text-white mt-6 font-sans text-3xl">Welcome back to Room.me!</h1>
+          <p className="text-white mt-4">Room.me is an innovative video conference product that revolutionizes virtual meetings.</p>
+          
+          {/* Form Section */}
+          <div className="mt-6 w-full">
+            <form className="w-full">
+              <label className="text-white font-semibold">Email Address</label>
+              <input 
+                type="email" 
+                placeholder="Enter your email address"
+                className="w-full p-3 mt-2 mb-3 text-white rounded-md border-2 border-gray-800 focus:outline-none focus:border-blue-500 bg-gray-900"
+              />
+              <label className="text-white font-semibold">Password</label>
+              <input 
+                type="password" 
+                placeholder="Enter your password"
+                className="w-full p-3 mt-2 text-white rounded-md border-2 border-gray-800 focus:outline-none focus:border-blue-500 bg-gray-900"
+              />
+              <button 
+                type="button" 
+                className="w-full p-3 mt-4 font-semibold text-white rounded-md border-2 border-gray-800 focus:outline-none focus:border-blue-500 hover:opacity-90 cursor-pointer"
+                style={{
+                  background: "linear-gradient(to right, #8B80FF, #5C53BC)"
+                }}
+              >
+                Sign In
+              </button>
+
+              <button 
+                type="submit" 
+                className="w-full font-semibold p-3 mt-4 text-black rounded-md border-2 border-gray-800 focus:outline-none focus:border-blue-500 hover:opacity-90 cursor-pointer"
+                style={{
+                  background: "white"
+                }}
+              >
+                Continue with Google
+              </button>
+
+              <div className="flex justify-between items-center">
+                <div className="flex items-center mt-4">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox text-blue-500 mr-2"
+                  />
+                  <label className="text-white font-light">Remember me for 30 days</label>
+                </div>
+
+                <div className="flex justify-end mt-2">
+                  <button className="text-sm text-purple-400 underline">
+                    Forgot password
+                  </button>
+                </div>
+              </div>
+              <div className="w-full flex justify-center mt-6">
+                <p className="text-white text-center">
+                  Doesn’t have an account? <span className="font-bold underline cursor-pointer">Sign Up</span>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+        
+        {/* Right Section */}
+        <div className="w-full md:w-1/2 mt-5 md:mt-0 relative">
+  
+          <Image 
+              src={image} 
+              alt="Image" 
+              className="opacity-60 rounded-2xl w-full h-full object-cover" 
+              style={{
+                background: "linear-gradient(to right, #6B61D2, #5C53BC)"
+              }}
+          />
+
+<div className="absolute bottom-10 left-0 w-full px-4 flex justify-center">
+  <div className="h-auto sm:h-64 backdrop-blur bg-white/10 border border-white/30 rounded-lg p-6 max-w-lg text-white">
+    <p className="mb-4 text-lg">
+      “We love the screen sharing and whiteboarding features, which have improved our presentations. Room.me has become an essential tool for our team, allowing us to collaborate effectively. Highly recommended!”
+    </p>
+    <p className="font-semibold text-sm text-white">Sarah Markivoc - Project Manager</p>
+  </div>
+</div>
+
+
+
+</div>
+
+      </div>
+    </div>
   );
 }
